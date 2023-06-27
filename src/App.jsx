db8345ca77bf7b78a6c89127ecdea2fa092d0b98
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2023-06-27 13:59:14 +0100

    add: DM Sans font

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='font-dm-sans'>
       <h1 className="text-center font-black text-6xl">Hello!</h1>
       <br />
         <button className="bg-slate-600 p-4 rounded-lg font-black text-white hover:bg-slate-400 duration-200" onClick={() => setCount((count) => count + 1)}>
           count is: {count}
         </button>
-        
-    </>
+    </div>
   )
 }
 
